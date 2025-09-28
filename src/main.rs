f57cd2debc40547b6dffd0847c46409030015624
0x5k <evil_b1t@icloud.com>
2025-09-28 21:45:17 +0200

    end of first post

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 #![no_std]
-
+#![no_main]
+//#![warn(dead_code)]
 use core::panic::PanicInfo;
+//use std::panic;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    //write!(DEBUG_OUTPUT, "panicked: {}", panic_info.message());
     loop {}
 }
 
-
-
-fn main() {
-   // println!("Hello, world!");
-    
-
-
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
